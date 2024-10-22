0c130b412708cf156440e3a5824effa01e6a449e
ajmaeen-98 <95845589+ajmaeenhelping@users.noreply.github.com>
2024-10-22 10:25:35 +0800

    Update and rename vid_ambassador.bat to execute.bat

---
+++ execute.bat
@@ -0,0 +1,2 @@
+"C:\{PATH TO YOUR chrome.exe}\Google\Chrome\Application\chrome.exe"  --autoplay-policy=no-user-gesture-required --kiosk --use-fake-ui-for-media-stream C:\{PATH TO WHERE YOUR PROJECT index.html is}\index.html
+pause
--- vid_ambassador.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\LENOVO\AppData\Local\Google\Chrome\Application\chrome.exe"  --autoplay-policy=no-user-gesture-required --kiosk --use-fake-ui-for-media-stream C:\ysl_kiosk\index.html
-pause
